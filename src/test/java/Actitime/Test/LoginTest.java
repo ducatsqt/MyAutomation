6e14e6b2caf7369af514a27d37b6eb684ed9965b
BIttu <praweenjha21@gmail.com>
2022-09-16 21:11:18 +0530

    this is login test added in master

--- src/test/java/Actitime/Test/LoginTest.java
+++ src/test/java/Actitime/Test/LoginTest.java
@@ -1,5 +1,25 @@
 package Actitime.Test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class LoginTest {
+	
+	@Test
+	public void testLogin() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dell\\Desktop\\Drivers\\latest\\chromedriver_win32 (18)\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://127.0.0.1/login.do");
+		Thread.sleep(5000);
+		System.out.println("Login page titile is-"+driver.getTitle());
+		driver.findElement(By.name("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.id("loginButton")).click();
+		driver.close();
+		
+	}
 
 }
