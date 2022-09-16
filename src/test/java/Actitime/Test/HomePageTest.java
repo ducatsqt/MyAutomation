0c043ec59d883e6c554afa8192cb3ca39a148258
BIttu <praweenjha21@gmail.com>
2022-09-16 21:07:02 +0530

    This is updated codes

--- src/test/java/Actitime/Test/HomePageTest.java
+++ src/test/java/Actitime/Test/HomePageTest.java
@@ -1,5 +1,26 @@
 package Actitime.Test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class HomePageTest {
+	
+	@Test
+	public void homePage() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dell\\Desktop\\Drivers\\latest\\chromedriver_win32 (18)\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://127.0.0.1/login.do");
+		driver.findElement(By.name("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.id("loginButton")).click();
+		Thread.sleep(5000);
+		System.out.println("Home page titile is-"+driver.getTitle());
+		driver.close();
+		
+		
+	}
 
 }
